2d86b362a113b32bd9c4c1daa333383319ad95a8
Rokel15 <dab.loam15@gmail.com>
2024-07-16 16:16:35 +0700

    update

--- agrotech/lib/data/images.dart
+++ agrotech/lib/data/images.dart
@@ -3,6 +3,4 @@ class Images{
   static String humiditySensor = "images/humidity-sensor.png";
   static String soilAnalysis = "images/soil-analysis.png";
   static String power = "images/power.png";
-  static String iconOn = "images/switch-on.png";
-  static String iconOff = "images/switch-off.png";
 }
