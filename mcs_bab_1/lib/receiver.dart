8d0c5cd7219393be91dec6f84ddffde737033c1f
Rokel15 <dab.loam15@gmail.com>
2024-08-28 13:59:59 +0700

    update

---
+++ mcs_bab_1/lib/receiver.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Receiver extends StatefulWidget {
+  String message;
+
+  Receiver({
+    super.key,
+    required this.message,
+  });
+
+  @override
+  State<Receiver> createState() => _ReceiverState();
+}
+
+class _ReceiverState extends State<Receiver> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text(widget.message),
+      ),
+    );
+  }
+}
