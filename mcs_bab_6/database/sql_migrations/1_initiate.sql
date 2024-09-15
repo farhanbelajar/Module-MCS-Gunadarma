0e2a7e06a77867343db93a284ae1d0884d4cb393
Rokel15 <dab.loam15@gmail.com>
2024-09-16 03:52:41 +0700

    update

---
+++ mcs_bab_6/database/sql_migrations/1_initiate.sql
@@ -0,0 +1,3 @@
+CREATE TABLE card(
+    id varchar(20)
+);
---
+++ mcs_bab_6/main.go
@@ -0,0 +1,13 @@
+package main
+
+// input
+// go get -u "github.com/gin-gonic/gin"
+// go get -u "github.com/lib/pq"
+// go get -u "github.com/rubenv/sql-migrate"
+// go get -u "github.com/joho/godotenv"
+
+import "fmt"
+
+func main() {
+	fmt.Println("test")
+}
